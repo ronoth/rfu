8935ffbc8ad775bf77b1b5a2ca85b72cfe796b37
Steven Osborn <osborn.steven@gmail.com>
2019-07-03 12:54:01 -0700

    option parser

---
+++ gui.cpp
@@ -0,0 +1,11 @@
+//
+// Created by steven on 7/3/2019.
+//
+
+#include <QApplication>
+#include <QDebug>
+
+int main(int argc, const char** argv) {
+    qDebug() << QT_VERSION_STR;
+    printf("HI\n");
+}
--- main.cpp
+++ main.cpp
@@ -1,52 +1,89 @@
+#include <algorithm>
 #include <iostream>
 #include <bitset>
 #include "Types.h"
 #include "CP210xRuntimeDLL.h"
 
+#include "cxxopts.hpp"
 
-int main(int argc, const char** argv) {
+void help() {
 
-    HANDLE hMasterCOM = CreateFile("\\\\.\\COM6", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, NULL);
+}
 
-    //1 2 4 8
-    CP210x_STATUS ret;
-    CP210xRT_WriteLatch(hMasterCOM, 0xffff, 0);
-    Sleep(100);
-    ret = CP210xRT_WriteLatch(hMasterCOM, 0x00ff, 0x02 | 0x01);
-    if(ret != CP210x_SUCCESS) {
-        std::cout << "ERROR Writing LATCH: " << ret << std::endl;
+int main(int argc, char** argv) {
+
+
+    cxxopts::Options options("Ronoth DFU", "Device Firmware Upgrade for devices by Ronoth, LLC");
+    options.add_options()
+            ("d,device", "Device Path / COM Port", cxxopts::value<std::string>())
+            ("h,help", "Display this message");
+
+    try {
+        auto opts = options.parse(argc, argv);
+
+
+        if (opts.count("help")) {
+            std::cout << options.help();
+            exit(1);
+        }
+        if (opts.count("device") != 1) {
+            std::cout << "Must specify serial device" << std::endl;
+            std::cout << options.help();
+            exit(1);
+        }
+
+        LPCSTR device = opts["device"].as<std::string>().c_str();
+
+        auto hMasterCOM = CreateFile(device, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_OVERLAPPED, NULL);
+
+
+        CP210xRT_WriteLatch(hMasterCOM, 0xffff, 0);
+        Sleep(100);
+        auto ret = CP210xRT_WriteLatch(hMasterCOM, 0x00ff, 1);
+        if(ret != CP210x_SUCCESS) {
+            std::cout << "ERROR Writing LATCH: " << ret << std::endl;
+        }
+        Sleep(100);
+
+
+        WORD value;
+        ret = CP210xRT_ReadLatch( hMasterCOM, &value);
+        if(ret != CP210x_SUCCESS) {
+            std::cout << "ERROR READING LATCH: " << ret << std::endl;
+        }
+
+        std::bitset<7> binary(value);
+        auto bstr = binary.to_string();
+        std::reverse(bstr.begin(), bstr.end());
+        std::cout <<  bstr << std::endl;
+
+
+        std::cout << "PRODUCT: ";
+        CP210x_PRODUCT_STRING product[CP210x_MAX_PRODUCT_STRLEN];
+        BYTE len = 0;
+        ret = CP210xRT_GetDeviceProductString(hMasterCOM, &product, &len, TRUE);
+        if(ret != CP210x_SUCCESS) {
+            std::cout << "ERROR READING Product String: " << ret << std::endl;
+        }
+        fwrite(product, sizeof(char), len, stdout);
+        std::cout << std::endl;
+
+        std::cout << "SERIAL: ";
+        CP210x_SERIAL_STRING serial[CP210x_MAX_SERIAL_STRLEN];
+        ret = CP210xRT_GetDeviceSerialNumber(hMasterCOM, &serial, &len, TRUE);
+        if(ret != CP210x_SUCCESS) {
+            std::cout << "ERROR READING Product String: " << ret << std::endl;
+        }
+        fwrite(serial, sizeof(char), len, stdout);
+        std::cout << std::endl;
+
+    } catch(cxxopts::OptionException& exception) {
+        std::cout << exception.what() << std::endl;
+        std::cout << options.help();
+        exit(1);
     }
-    Sleep(100);
 
 
-    WORD value;
-    ret = CP210xRT_ReadLatch( hMasterCOM, &value);
-    if(ret != CP210x_SUCCESS) {
-        std::cout << "ERROR READING LATCH: " << ret << std::endl;
-    }
-
-    std::bitset<8> binary(value);
-    std::cout << binary << std::endl;
-
-
-    std::cout << "PRODUCT: ";
-    CP210x_PRODUCT_STRING product[CP210x_MAX_PRODUCT_STRLEN];
-    BYTE len = 0;
-    ret = CP210xRT_GetDeviceProductString(hMasterCOM, &product, &len, TRUE);
-    if(ret != CP210x_SUCCESS) {
-        std::cout << "ERROR READING Product String: " << ret << std::endl;
-    }
-    fwrite(product, sizeof(char), len, stdout);
-    std::cout << std::endl;
-
-    std::cout << "SERIAL: ";
-    CP210x_SERIAL_STRING serial[CP210x_MAX_SERIAL_STRLEN];
-    ret = CP210xRT_GetDeviceSerialNumber(hMasterCOM, &serial, &len, TRUE);
-    if(ret != CP210x_SUCCESS) {
-        std::cout << "ERROR READING Product String: " << ret << std::endl;
-    }
-    fwrite(serial, sizeof(char), len, stdout);
-    std::cout << std::endl;
 
     return 0;
 }
